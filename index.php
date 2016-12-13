64358d714b124266b16f87c0eea57d9fa99351ca
mumugreen <philtyphils@gmail.com>
2016-12-14 02:22:47 +0700

    Header Refresh To Paypal

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    header('X-Frame-Options: GOFORIT'); 
+    header('X-Frame-Options: SAMEORIGN'); 
 ?>
 <html>
 <head>
@@ -8,9 +8,9 @@
 <meta name="keywords" content="Philtyphil Blog, Paypal, Intro Pages, sketsablog.com">
 <meta name="description" content="This My New Philtyphil Blog Website. Instead, The website still under concept but I was buying domain. So I regristed the sketsablog.com to be my own domain, and make Paypal donate To My Intro Page. This Website Will Be ready as soon as possible."/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta http-equiv="refresh" content="1;url=https://www.paypal.me/philtyphil" />
 </head>
 <body>
-<iframe width="100%" height="100%" src="http://paypal.me/philtyphil"/>
 </body>
 <footer>
 </footer>
