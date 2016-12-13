c6f489551a7a6ed720b801cfb052a416853e5259
mumugreen <philtyphils@gmail.com>
2016-12-14 02:12:14 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<meta charset="UTF-8"/>
+<meta name="author" name="Sulistyo N. Anggoro - @philtyphils"/>
+<meta name="keywords" content="Philtyphil Blog, Paypal, Intro Pages, sketsablog.com">
+<meta name="description" content="This My New Philtyphil Blog Website. Instead, The website still under concept but I was buying domain. So I regristed the sketsablog.com to be my own domain, and make Paypal donate To My Intro Page. This Website Will Be ready as soon as possible."/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+<iframe width="100%" height="100%" src="http://paypal.me/philtyphil"/>
+</body>
+<footer>
+</footer>
+</html>
