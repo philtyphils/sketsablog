42c4212fc1a55436dca3c2668fdc79fd62da18a7
mumugreen <philtyphils@gmail.com>
2016-12-23 14:49:41 +0700

    change Title and others

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
-
+        <title>SketsaBlog.com | @philtyphils</title>
+		<meta charset="UTF-8">
+		<meta name="description" content="SketsaBlog Under Build. Will be launch soon. ">
+		<meta name="keywords" content="SketsaBlog.com">
+		<meta name="author" content="Sulistyo Nur Anggoro -@philtyphils">
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
+		
         <style>
             html, body {
                 height: 100%;
